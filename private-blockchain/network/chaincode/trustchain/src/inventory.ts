3cd23c4915e29908a91947da63aea7dbfbbc07f1
Md Antonin Islam <antonin.islam.dev@gmail.com>
2023-07-12 20:25:22 +0600

    9 - Adds inventory class

---
+++ private-blockchain/network/chaincode/trustchain/src/inventory.ts
@@ -0,0 +1,7 @@
+export class Inventory {
+    public title: string;
+    public description: string;
+    public location: any;
+    public owner: string;
+    public external: any;
+}
--- private-blockchain/network/chaincode/trustchain/src/product.ts
+++ private-blockchain/network/chaincode/trustchain/src/product.ts
@@ -1,13 +1,10 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
 export class Product {
     public title: string;
     public description: string;
     public mfg_id?: string;
     public mfg_licence: string;
     public owner: string;
+    public lastestHolderKey: string;
     public mint_date?: string;
     public external: any;
 }
