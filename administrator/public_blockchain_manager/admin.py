1bd623ef0c2fa58c4a8b518b8dd5951eaf302b4a
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-07-15 17:25:06 +0600

    6-Public Blockchain Admin Visibility Updated

--- administrator/public_blockchain_manager/admin.py
+++ administrator/public_blockchain_manager/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from public_blockchain_manager.models import SmartContractDeployment, SignTypeData, EthSignature
 
 
 # Register your models here.
 @admin.register(SmartContractDeployment)
 class SmartContractDeploymentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'address', 'network')
+    def network_url(self, obj):
+        return format_html("<a href='{url}' target='_blank'>{title} 🖧</a>", url=obj.network, title = obj.network_title)
+    
+    def contract_address(self, obj):
+        return format_html("<a href='{url}/address/{address}' target='_blank'>📜 {address}</a>", url=obj.network, address = obj.address)
+    list_display = ('title', 'contract_address', 'version', 'network_url', 'is_verified', 'is_proxy')
     
 
 @admin.register(SignTypeData)
