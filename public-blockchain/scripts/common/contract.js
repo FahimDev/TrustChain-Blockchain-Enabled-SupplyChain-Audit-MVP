81dcebdabece56f7bc8ad45088351e7e49d64b05
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-07-26 15:22:20 +0600

    5-Gets Contract Instance with ABI and Connects Wallet

---
+++ public-blockchain/scripts/common/contract.js
@@ -0,0 +1,13 @@
+const { ethers } = require('ethers');
+const { getPricingABI } = require('./file');
+
+const getInstance = (
+    address,
+    wallet
+  ) => {
+    const abi = getPricingABI()
+    const contract = new ethers.Contract(address, abi, wallet)
+    return contract
+}
+
+module.exports = {getInstance};
